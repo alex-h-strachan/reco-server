0e53463e113615ff6e3a32294bbb5e9f68010bd3
Alex Strachan <alexst@outlook.com>
2017-01-19 15:17:28 -0800

    skelleton for recommendation view

--- test/views/recomendation.js
+++ test/views/recomendation.js
@@ -2,14 +2,22 @@
 /*global describe*/
 /*global it*/
 
-var recommendation;
+var recommendation,
+    subtopics;
 
 before(function() {
+    this.timeout(0);
     recommendation = require('../../views/recommendation');
+    subtopics = require('../../data/subtopics.json');
 });
 
 describe('recommendation', function() {
-    it('should work', function(done) {
-        done();
+    it('should return a recommendation for a valid category', done => {
+        var recs = recommendation(subtopics[0].id, 10);
+        if(recs.length > 0) {
+            return done();
+        } else {
+            return done(new Error('recommendations weren\'t generated'));
+        }
     });
 });
--- views/recommendation.js
+++ views/recommendation.js
@@ -1,1 +1,8 @@
 var subtopicRelation = require('../controllers/subtopicRelation');
+
+function getRecommendations(subtopic, limit = 10) {
+    var relations = subtopicRelation.getRelation(subtopic);
+    return relations;
+}
+
+module.exports = getRecommendations;
