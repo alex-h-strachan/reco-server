4ca1e5aebff59ba7c4cb4f63c567588f98702c4b
Alex Strachan <alexst@outlook.com>
2017-01-19 12:29:08 -0800

    describe subtopicRelation return format

--- controllers/subtopicRelation.js
+++ controllers/subtopicRelation.js
@@ -1,5 +1,17 @@
-function subtopicRelations(subtopicID) {
-    
+class Relation {
+    constructor(parent, subtopic, linkPercent) {
+        this.parent = parent;
+        this.subtopic = subtopic;
+        this.linkPercent = linkPercent;
+    }
+}
+
+function subtopicRelations(subtopic) {
+    var relations = [
+        new Relation(subtopic, 1, 1)
+    ];
+
+    return relations;
 }
 
 module.exports = subtopicRelations;
--- test/controllers/subtopicRelation.js
+++ test/controllers/subtopicRelation.js
@@ -2,14 +2,29 @@
 /*global describe*/
 /*global it*/
 
-var subtopicRelation;
+var subtopicRelation, 
+    subtopicData;
 
 before(function() {
     subtopicRelation = require('../../controllers/subtopicRelation.js');
+    subtopicData = require('../../data/subtopics.json');
 });
 
 describe('subtopicRelation', function() {
-    it('should work', function(done) {
-        done();
+    it('should take a subtopic and return a set of relations', function(done) {
+        var relations = subtopicRelation(subtopicData[0]);
+        if(
+            Array.isArray(relations) 
+            && relations[0] 
+            && relations[0].linkPercent
+            && relations[0].subtopic
+            && relations[0].parent
+        ) {
+            return done();
+        } else {
+            return done(
+                new Error('Subtopic return format malformed')
+            );
+        }
     });
 });
