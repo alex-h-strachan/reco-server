989d5f13c3538f66d981008707e14660f424910e
Alex Strachan <alexst@outlook.com>
2017-01-19 17:38:02 -0800

    better eslint rule style

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console*/
+
 var config = require('./config.json');
 var app = require('./app');
 
 app.listen(config.port, function () {
-    // eslint-disable-next-line no-console
     console.log(`Server started on ${config.port}`);
+    console.log(`try it out: go to localhost:${config.port}/recommendations?subtopic=57d80b25e04b0603003bffa3&limit=3&pretty=true`);
 });
